6df6e593954aa3aa61ef028eaa7eb59ed1bb5193
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-05-07 16:01:44 +0300

    task 4-1 final(1)

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -24,7 +24,7 @@ public class DaysInMonth {
                 System.out.println("31");
                 break;
             default:
-                System.out.println("wrong number!");
+                System.out.println("invalid date");
                 break;
         }
     }
